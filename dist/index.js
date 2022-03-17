d09fe4909398c2aedd9b30ce6d261da5330f4357
SDxBacon <z141134553@gmail.com>
2022-03-18 01:12:19 +0800

    feat: 🎸 簡單把邏輯統整

--- dist/index.js
+++ dist/index.js
@@ -1,71 +1,45 @@
 "use strict";
-window.addEventListener('load', function () {
-    const Answer = document.getElementById("Answer");
-    if (Answer != null) {
-        Answer.innerHTML = '0';
+/**
+ * class AnswerClass
+ * TODO: getValue & appendVal 會有溢位問題
+ */
+class AnswerClass {
+    constructor() {
+        this.el = document.getElementById("Answer");
     }
-    let number = 0;
-    const one = document.getElementById("one");
-    const tue = document.getElementById("two");
-    const three = document.getElementById("three");
-    const four = document.getElementById("four");
-    const five = document.getElementById("five");
-    const six = document.getElementById("six");
-    const seven = document.getElementById("seven");
-    const eight = document.getElementById("eight");
-    const nine = document.getElementById("nine");
-    one === null || one === void 0 ? void 0 : one.addEventListener('click', function () {
-        number = number * 10 + 1;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
+    getValue() {
+        if (this.el !== null) {
+            return Number(this.el.innerHTML);
         }
-    });
-    tue === null || tue === void 0 ? void 0 : tue.addEventListener('click', function () {
-        number = number * 10 + 2;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
+        return 0;
+    }
+    appendVal(val) {
+        if (this.el !== null) {
+            const currVal = this.getValue();
+            const nextVal = currVal * 10 + val;
+            this.el.innerHTML = nextVal.toString();
         }
-    });
-    three === null || three === void 0 ? void 0 : three.addEventListener('click', function () {
-        number = number * 10 + 3;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
+    }
+}
+/**
+ * onload
+ */
+window.addEventListener("load", function () {
+    // initialize answerObj
+    const answerObj = new AnswerClass();
+    /**
+     * number element onClick handler
+     */
+    function numberElementOnClickHander(evt) {
+        if (evt.target instanceof HTMLButtonElement) {
+            const valueAttr = evt.target.getAttribute("value");
+            const numValueAttr = Number(valueAttr);
+            answerObj.appendVal(numValueAttr);
         }
-    });
-    four === null || four === void 0 ? void 0 : four.addEventListener('click', function () {
-        number = number * 10 + 4;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
-    five === null || five === void 0 ? void 0 : five.addEventListener('click', function () {
-        number = number * 10 + 5;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
-    six === null || six === void 0 ? void 0 : six.addEventListener('click', function () {
-        number = number * 10 + 6;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
-    seven === null || seven === void 0 ? void 0 : seven.addEventListener('click', function () {
-        number = number * 10 + 7;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
-    eight === null || eight === void 0 ? void 0 : eight.addEventListener('click', function () {
-        number = number * 10 + 8;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
-    nine === null || nine === void 0 ? void 0 : nine.addEventListener('click', function () {
-        number = number * 10 + 9;
-        if (Answer != null) {
-            Answer.innerHTML = number.toString();
-        }
-    });
+    }
+    // mount click event handlers
+    const numberElements = document.getElementsByClassName("number");
+    for (const el of numberElements) {
+        el.addEventListener("click", numberElementOnClickHander);
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,51 @@
-window.addEventListener('load', function () {
-const Answer = document.getElementById("Answer")
-if (Answer != null){Answer.innerHTML = '0'}
-let number:number = 0
+/**
+ * class AnswerClass
+ * TODO: getValue & appendVal 會有溢位問題
+ */
+class AnswerClass {
+  private el;
 
-const one = document.getElementById("one")
-const tue = document.getElementById("two")
-const three = document.getElementById("three")
-const four = document.getElementById("four")
-const five = document.getElementById("five")
-const six = document.getElementById("six")
-const seven = document.getElementById("seven")
-const eight = document.getElementById("eight")
-const nine = document.getElementById("nine")
+  constructor() {
+    this.el = document.getElementById("Answer");
+  }
 
-one?.addEventListener('click' , function (){
-    number = number*10+1
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-tue?.addEventListener('click' , function (){
-    number = number*10+2
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-three?.addEventListener('click' , function (){
-    number = number*10+3
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-four?.addEventListener('click' , function (){
-    number = number*10+4
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-five?.addEventListener('click' , function (){
-    number = number*10+5
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-six?.addEventListener('click' , function (){
-    number = number*10+6
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-seven?.addEventListener('click' , function (){
-    number = number*10+7
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-eight?.addEventListener('click' , function (){
-    number = number*10+8
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-nine?.addEventListener('click' , function (){
-    number =  number*10+9
-    if (Answer != null){Answer.innerHTML = number.toString()}
-})
-})
+  public getValue() {
+    if (this.el !== null) {
+      return Number(this.el.innerHTML);
+    }
+    return 0;
+  }
+
+  public appendVal(val: number) {
+    if (this.el !== null) {
+      const currVal = this.getValue();
+      const nextVal = currVal * 10 + val;
+      this.el.innerHTML = nextVal.toString();
+    }
+  }
+}
+
+/**
+ * onload
+ */
+window.addEventListener("load", function () {
+  // initialize answerObj
+  const answerObj = new AnswerClass();
+
+  /**
+   * number element onClick handler
+   */
+  function numberElementOnClickHander(evt: Event) {
+    if (evt.target instanceof HTMLButtonElement) {
+      const valueAttr = evt.target.getAttribute("value");
+      const numValueAttr = Number(valueAttr);
+      answerObj.appendVal(numValueAttr);
+    }
+  }
+
+  // mount click event handlers
+  const numberElements = document.getElementsByClassName("number");
+  for (const el of numberElements) {
+    el.addEventListener("click", numberElementOnClickHander);
+  }
+});
